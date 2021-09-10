60b50ccda897916abc9094e3771879faca087ada
Macbook <makich.markosyan@bk.ru>
2021-09-11 02:37:14 +0400

    Initial commit

---
+++ src/HomeWork3.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+//        Task 1
+//        Scanner NumberScan = new Scanner(System.in);
+//        System.out.println("Enter your first number ");
+//        int number = NumberScan.nextInt();
+//        System.out.println("select a symbol to define the operation 'a - add','s - subtract','m - multiply','d - divide': ");
+//        String operation = NumberScan.next();
+//        System.out.println("Enter second number");
+//        int number2 = NumberScan.nextInt();
+//        switch (operation) {
+//            case "a":
+//                System.out.println("Result of add is = " + (number + number2));
+//                break;
+//            case "b":
+//                System.out.println("Result of subtract is = " + (number - number2));
+//                break;
+//            case "m":
+//                System.out.println("Result of multiply is = " + number * number2);
+//                break;
+//            case "d":
+//                if (number2 == 0)
+//                    System.out.println("Тhe selected divide operation is incorrect, please try again");
+//                else {
+//                    System.out.println("Result of divide = " + number / number2);
+//                }
+//                break;
+//            default:
+//                System.out.println("Error,please try again");
+//        }
+// Task 2
+        Scanner i = new Scanner(System.in);
+        System.out.println("Select your floor");
+        int floor =  i.nextInt();
+        switch (floor) {
+            case 1:
+                System.out.println("Go to 1 floor");
+                break;
+            case 2:
+                System.out.println("Go to 2 floor");
+                break;
+            case 3:
+                System.out.println("Go to 3 floor");
+                break;
+            case 4:
+                System.out.println("Go to 4 floor");
+                break;
+            case 5:
+                System.out.println("Go to 5 floor");
+                break;
+            default:
+                System.out.println("Going to parking ");
+        }
+    }
+}
